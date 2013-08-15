e9aecc55ca85550209f65297f4954f4162239069
Dan Holloran <dholloran@matchboxdesigngroup.com>
2013-08-14 20:31:44 -0500

    Cleaned up the admin js and sorted js files

--- assets/js/site/main.js
+++ assets/js/site/main.js
@@ -2,6 +2,7 @@
 // @codekit-prepend "vendor/imagesloaded.js"
 // @codekit-prepend "vendor/plugin.js"
 // @codekit-prepend "vendor/jquery.flexslider.js"
+// @codekit-prepend "src/_functions.js"
 jQuery(function(){
 
 }($));
