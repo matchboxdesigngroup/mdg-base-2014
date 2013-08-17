51f3b4650e41c041e13c22f548274e9a0aaeff38
Dan Holloran <dholloran@matchboxdesigngroup.com>
2013-08-17 13:36:51 -0500

    Added navbar drop down setup fixes #4

--- assets/js/site/main.js
+++ assets/js/site/main.js
@@ -2,7 +2,32 @@
 // @codekit-prepend "vendor/imagesloaded.js"
 // @codekit-prepend "vendor/plugin.js"
 // @codekit-prepend "vendor/jquery.flexslider.js"
-// @codekit-prepend "src/_functions.js"
 jQuery(function(){
+	var generic = {},
+			navMenu = {}
+	;
 
+	/**
+	 * Initializes the primary drop down menu to make it work
+	 * on hover instead of the default Bootstrap on click
+	 *
+	 * @return boolean
+	 */
+	navMenu.initDropDownMenu = function( dropdown ) {
+		dropdown = (typeof dropdown === "undefined") ? $('.dropdown') : dropdown;
+
+		// Activate Drop Down on Toggle
+		dropdown.on('mouseover', function () {
+			$(this).addClass('open');
+		}).on('mouseout', function(){
+			$(this).removeClass('open');
+		});
+
+		return false;
+	}; // initDropDownMenu()
+
+	// Window Load
+	$(window).load(function(){
+		navMenu.initDropDownMenu();
+	}); // $(window).load()
 }($));
