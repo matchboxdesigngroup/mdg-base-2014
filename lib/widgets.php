f740695892fe308e6abf395047938e50a44f5ea4
Dan Holloran <dholloran@matchboxdesigngroup.com>
2013-12-07 09:39:26 -0600

    Added default widget area fixes #19

--- lib/widgets.php
+++ lib/widgets.php
@@ -4,16 +4,16 @@
  */
 function roots_widgets_init() {
 	// Sidebars
-	// register_sidebar(
-	// 	array(
-	// 		'name'          => __( 'Primary', 'roots' ),
-	// 		'id'            => 'sidebar-primary',
-	// 		'before_widget' => '<section class="widget %1$s %2$s"><div class="widget-inner">',
-	// 		'after_widget'  => '</div></section>',
-	// 		'before_title'  => '<h3>',
-	// 		'after_title'   => '</h3>',
-	// 	)
-	// );
+	register_sidebar(
+		array(
+			'name'          => __( 'Primary', 'roots' ),
+			'id'            => 'sidebar-primary',
+			'before_widget' => '<section class="widget %1$s %2$s"><div class="widget-inner">',
+			'after_widget'  => '</div></section>',
+			'before_title'  => '<h3>',
+			'after_title'   => '</h3>',
+		)
+	);
 
 	// Widgets
 	// register_widget( 'Roots_Vcard_Widget' );
