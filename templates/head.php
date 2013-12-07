7c54f2ab72dd6586a4bc73322a243731bc0e01e0
Dan Holloran <dholloran@matchboxdesigngroup.com>
2013-12-07 09:37:18 -0600

    Added fix for IE10 viewport bug when using Bootstrap

--- templates/head.php
+++ templates/head.php
@@ -7,7 +7,7 @@
 	<meta charset="utf-8">
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<?php wp_head(); ?>
 
 	<link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo( 'name' ); ?> Feed" href="<?php echo home_url(); ?>/feed/">
