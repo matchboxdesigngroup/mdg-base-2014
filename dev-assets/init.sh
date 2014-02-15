d3b2379078e4de0270f5b42f83656fd8fe46015d
Dan Holloran <dholloran@matchboxdesigngroup.com>
2014-02-15 10:19:35 -0600

    Added linking of JSCS to init.sh

--- dev-assets/init.sh
+++ dev-assets/init.sh
@@ -11,6 +11,10 @@ echo "\nUpdating theme Composer requirements";
 cd ../;
 composer update
 
+echo "\nLinking up JSCS configuration files";
+ln -s .jscs.json assets/js/src/site/.jscs.json;
+ln -s .jscs.json assets/js/src/admin/.jscs.json;
+
 echo "\nInstalling development WordPress plugins"
 cp dev-assets/composer-plugins.json ../../../composer.json;
 cd ../../../;
