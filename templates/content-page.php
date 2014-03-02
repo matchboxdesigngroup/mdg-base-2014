0caed82c2ebce74edebd66746b086414f06fbc4c
Dan Holloran <dholloran@matchboxdesigngroup.com>
2014-03-02 17:51:15 -0600

    Removed converted templates

--- templates/content-page.php
+++
@@ -1,7 +0,0 @@
-<div class="container">
-	<?php while ( have_posts() ) {
-		the_post();
-		the_content();
-		wp_link_pages( array( 'before' => '<nav class="pagination">', 'after' => '</nav>' ) );
-	} // while() ?>
-</div> <!-- /.container -->
