b3bf12ebd7f2a6c7c697da91711ef9d0809f00ee
Dan Holloran <dholloran@matchboxdesigngroup.com>
2014-03-08 12:22:12 -0600

    Added PHPDoc grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,8 +9,19 @@ module.exports = function(grunt) {
 	// This will load all grunt-* tasks that are in the package.json devDependencies
 	require('load-grunt-tasks')(grunt, { scope: 'devDependencies' });
 
+	/**
+	 * Runs the phpdoc command to generate  PHPDocumentation.
+	 *
+	 * @return  {void}
+	 */
+	grunt.registerTask('phpdoc', 'Executes PHPDocumentor command.', function() {
+		var exec = require('child_process').exec;
+
+		exec('phpdoc', this.async());
+	});
+
 	// Register Tasks
 	grunt.registerTask('default', [ 'watch' ]);
 	grunt.registerTask('conflict', [ 'sass:site', 'sass:ltie9', 'uglify:site' ]);
-	grunt.registerTask('build', [ 'sass', 'imagemin', 'uglify:build', 'autoprefixer', 'group_css_media_queries', 'cssmin' ]);
+	grunt.registerTask('build', [ 'sass', 'imagemin', 'uglify:build', 'autoprefixer', 'group_css_media_queries', 'cssmin', 'phpdoc' ]);
 };
