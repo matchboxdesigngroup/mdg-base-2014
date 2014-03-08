d3747ed4832c14869642ea14d27fc9643eaf57ec
Dan Holloran <dholloran@matchboxdesigngroup.com>
2014-03-07 18:48:16 -0600

    Added grunt task for when there is a conflict

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,9 @@
 module.exports = function(grunt) {
 	// Measures the time each task takes
-  require('time-grunt')(grunt);
+	require('time-grunt')(grunt);
 
 	// Load Grunt Configurations
-  require('load-grunt-config')(grunt);
+	require('load-grunt-config')(grunt);
 
 	// Load Tasks
 	// This will load all grunt-* tasks that are in the package.json devDependencies
@@ -11,4 +11,5 @@ module.exports = function(grunt) {
 
 	// Register Tasks
 	grunt.registerTask('default', [ 'watch' ]);
+	grunt.registerTask('conflict', [ 'sass:site', 'sass:ltie9', 'uglify:site' ]);
 };
